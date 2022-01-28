469e262c79b75e2b447ac12740e8d3f959fef291
Gayathrig-mediwave <gayathridevi_g@mindwaveventures.com>
2022-01-28 18:00:40 +0530

    after crud operations

--- app.js
+++ app.js
@@ -1,3 +1,56 @@
 const express = require("express");
 const port = 8080;
 const app = express();
+const db = require("./db");
+app.use(express.json());
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+    console.log(`cannot run on${port}`);
+    PromiseRejectionEvent.exit(1);
+  }
+  console.log(`server running on port${port}`);
+});
+//empty page
+app.get("/", (req, res) => {
+  return res.send("hello");
+});
+//show all movies
+app.get("/movies", (req, res) => {
+  const movies = db.movies;
+  return res.send(movies);
+});
+//show one movie
+app.get("/movies/:id", (req, res) => {
+  //console.log("params:", req.params.id);
+  //return res.send(db.movies);
+  const movieId = req.params.id;
+  //const movie = db.movies.find((m) => m.id == movieId);
+  const movie = db.getOneMovie(movieId);
+  if (!movie) {
+    return res.status(404).send({
+      message: `movie ${movieId}not found`,
+    });
+  }
+  return res.send(movie);
+});
+//add a movie
+app.post("/movies", (req, res) => {
+  const movie = db.addMovie(req.body);
+  return res.send(movie);
+});
+//delete a movie
+app.delete("/movies", (req, res) => {
+  const movieId = req.params.id;
+  const movie = db.deleteMovie(movieId);
+  return res.send({
+    message: "movie is deleted",
+  });
+});
+//update a movie
+app.put("/movies", (req, res) => {
+  const movieId = req.params.id;
+  const movie = db.updateMovie(movieId, req.body);
+  return res.send(movie);
+});
---
+++ db.js
@@ -0,0 +1,48 @@
+const movies = [
+  {
+    id: 1643102154540,
+    name: "Tangled",
+  },
+  {
+    id: 1643102154541,
+    name: "The Princess",
+  },
+];
+const getOneMovie = (movieId) => {
+  const movie = movies.find((m) => m.id == movieId);
+  return movie;
+};
+
+const addMovie = (payload) => {
+  const movie = {
+    id: new Date().getTime(),
+    name: payload.name,
+  };
+  movies.push(movie);
+  return movie;
+};
+
+const deleteMovie = (movieId) => {
+  const movieIndex = movies.findIndex((m) => m.id == movieId);
+  if (movieIndex != -1) {
+    movies.splice(movieIndex, 1);
+  } else {
+    console.log(`movie id ${moviId} cannot be deleted`);
+  }
+};
+
+const updateMovie = (movieId, payload) => {
+  const movieIndex = movies.findIndex((m) => m.id == movieId);
+  if (movieIndex != -1) {
+    movies[movieIndex][name] = payload.name;
+  } else {
+    console.log(`movie id ${moviId} cannot be deleted`);
+  }
+};
+module.exports = {
+  movies,
+  getOneMovie,
+  addMovie,
+  deleteMovie,
+  updateMovie,
+};
